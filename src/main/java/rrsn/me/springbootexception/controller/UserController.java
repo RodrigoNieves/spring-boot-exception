cb4e3593fa44cc90978cdd7fd658bd8efdb4e7ed
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2019-11-10 11:48:10 -0800

    Add general Exceptions controller.

--- src/main/java/rrsn/me/springbootexception/controller/UserController.java
+++ src/main/java/rrsn/me/springbootexception/controller/UserController.java
@@ -20,20 +20,4 @@ public class UserController {
         name = name.toLowerCase(); // this line will cause NullPointerException because it calls a method on a null object
         return "update";
     }
-
-    @ExceptionHandler(value={java.lang.ArithmeticException.class})
-    public ModelAndView handlerArithmeticException(Exception e) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("exception", e.toString());
-        modelAndView.setViewName("mathError");
-        return modelAndView;
-    }
-
-    @ExceptionHandler(value={java.lang.NullPointerException.class})
-    public ModelAndView handlerNullPointerException(Exception e) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("exception", e.toString());
-        modelAndView.setViewName("nullPointerError");
-        return modelAndView;
-    }
 }
