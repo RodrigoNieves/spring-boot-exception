3d89d2502df0c953794ce01278dc998c8008465e
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2019-11-10 11:00:49 -0800

    Initialize start.spring.io with Spring Web, Spring Boot DevTools and Thymeleaf.

---
+++ src/test/java/rrsn/me/springbootexception/SpringBootExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package rrsn.me.springbootexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
